62287c9b1bf61eb6ea2efd2444ef68d4af60fd69
Jonathan Borja <jborja.one@gmail.com>
2024-11-25 19:03:34 -0700

    part 1

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -1,12 +1,18 @@
+using System.Data;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
-var builder = WebApplication.CreateBuilder(args);
+using MySql.Data.MySqlClient;using Testing;var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddScoped<IDbConnection>((s) =>
+{
+    IDbConnection conn = new MySqlConnection(builder.Configuration.GetConnectionString("bestbuy"));
+    conn.Open();
+    return conn;
+});
+builder.Services.AddTransient<IProductRepository, ProductRepository>();
 var app = builder.Build();
 
 
