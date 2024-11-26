2975ff90fda566fed4e5dea92b4bf342f6dadc44
Jonathan Borja <jborja.one@gmail.com>
2024-11-25 19:19:42 -0700

    part 3

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -1,1 +1,1 @@
-﻿using System;using System.Collections.Generic;using Testing.Models;namespace Testing{    public interface IProductRepository    {        IEnumerable<Product> GetAllProducts();        Product GetProduct(int id);    }}
+﻿using System;using System.Collections.Generic;using Testing.Models;namespace Testing{    public interface IProductRepository    {        IEnumerable<Product> GetAllProducts();        Product GetProduct(int id);        void UpdateProduct(Product product);    }}
